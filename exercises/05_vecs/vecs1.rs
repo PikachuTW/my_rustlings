ccc6b229d9fdab777d94a7c27669bad972302c2e
PikachuTW <chenyenting0830@gmail.com>
2024-11-16 11:25:29 +0800

    completed 05_vecs

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -5,6 +5,11 @@ fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     // Use the vector macro.
     // let v = ???;
 
+    let mut v = Vec::new();
+    for element in &a {
+        v.push(*element);
+    }
+
     (a, v)
 }
 
