5bd4a8f66fa4f54b0bbc511a4fdc26d1807aae50
PikachuTW <chenyenting0830@gmail.com>
2024-11-16 11:07:04 +0800

    completed quiz_1 and 04_primitive_types

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -11,6 +11,13 @@
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(original_price: i32) -> i32 {
+    if original_price > 40 {
+        original_price
+    } else {
+        original_price * 2
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
